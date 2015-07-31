2150f41f2f61f64517a0846b139131eec7ee7832
Rachel <rachmklein@gmail.com>
2015-07-31 09:29:47 -0700

    Header is functional

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,26 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Rachel Klein";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Web Design & Development";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
+var skills = ["HTML5","CSS","Photoshop","JavaScript"];
+
+var bio = {
+	"name": name,
+	"role": role,
+	"skills": skills,
+	"contacts": {
+		"mobile": "801-597-6276",
+		"email": "rachmklein@gmail.com",
+		"githib": "elleKlein",
+		"location": "San Francisco",
+	},
+	"bioPic": "images/moi.jpg",
+	"welcome message": "Hi there! Thank you for stopping by!"
+}
+
+$("#main").append(bio.contacts.mobile);
